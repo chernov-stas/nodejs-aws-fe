0f5563aed3952adb569159eeb82a5ac123402c62
Stanislav_Chernov <Stanislav_Chernov@epam.com>
2020-11-15 17:58:36 +0600

    [task-5] Changed url for import endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://7gxu8tjgcd.execute-api.eu-west-1.amazonaws.com/dev/products',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://tfbf0r0kod.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://7gxu8tjgcd.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
